542d61e1a3f0fd06e8538640999a923390700e61
skyujilong <skyujilong@163.com>
2017-12-08 18:11:11 +0800

    添加ejs模板

--- app/index.js
+++ app/index.js
@@ -5,12 +5,26 @@ const app = koa();
 const router = require('koa-router')();
 const serve = require('koa-static');
 const path = require('path');
+const render = require('koa-ejs');
 //中间件配置位置
 app.use(logger());
+//ejs配置
+render(app, {
+    root: path.join(__dirname, 'static'),
+    layout: false,
+    viewExt: 'html',
+    cache: false,
+    debug: true,
+    open:'{{',
+    close:'}}'
+});
 // app.use(serve(path.resolve(__dirname, '..', 'static', 'test')));
 // app.use(serve(path.resolve(__dirname, '..', 'static', 'dll')));
 router.get('/',function * (next){
-    this.body = 'hello world';
+    yield this.render('index',{data:{
+        text:'<span>hello world</span>',
+        num:1
+    }});
     yield next;
 });
 app.use(router.routes());
