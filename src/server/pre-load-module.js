f662036482e42dd0cc6ccb7e1575ca1834b39e9a
skyujilong <skyujilong@163.com>
2018-01-23 17:51:00 +0800

    添加相关解析内容

--- src/server/pre-load-module.js
+++ src/server/pre-load-module.js
@@ -17,35 +17,20 @@ __webpack_require__.e(0).then(__webpack_require__.bind(null,35)).then((m)=>{
 export default co(function *(){
     let files = yield thunkify(fs.readdir)(path.resolve(__dirname,'js'));
     let modulesNameReg = /^(.*)-chunk-[a-f0-9]{6}\.js$/;
+    //获取异步加载模块的id
+    let eIdReg = /exports.ids\s=\s\[(\d+)\]/;
+    //模块的id
+    let moduleId = /\/\*\*\*\/\s+(\d+):/;
+    let moduleList = [];
     for(let fileName of files){
         console.log(fileName);
         let result = modulesNameReg.exec(files);
-        if (result && result[0]) {
-            console.log(result[1]);
+        if (!result) {
+            continue;
         }
-        // let preloadModule = require('js/' + fileName);
-        // console.log(preloadModule.default);
         let codeContext = yield thunkify(fs.readFile)('js/' + fileName,'utf-8');
-        // console.log(codeContext);
-        (new vm.Script(`
-            try{
-                ${codeContext}
-                console.log('xxxxx');
-            }catch(e){
-                console.log(e.stack);
-            }
-        `)).runInThisContext();
-
-        // let moduleScript = new vm.Script('let a = "22222"');
-        // moduleScript.runInThisContext();
-        // console.log(a);
-        // (new vm.Script(`
-        //     try{
-        //         console.log(__webpack_require__)
-        //     }catch(e){
-        //         console.log(e.stack);
-        //     }
-        // `)).runInThisContext();
+        console.log(codeContext);
+        console.log(codeContext.split(/\n/));
     }
     
 });
