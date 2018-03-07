ff2221fc050a320d3e9209d7f9e3898fff6a44ca
skyujilong <skyujilong@163.com>
2018-03-07 18:08:06 +0800

    添加一个测试ie的例子，回头看看是否ie8上的defineProperty上的问题 比较严重

--- src/client/js/mods/ui/Home/index.jsx
+++ src/client/js/mods/ui/Home/index.jsx
@@ -4,17 +4,37 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import {getArticle} from '../../data/action'
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.textChange = this.textChange.bind(this);
+        this.state = {
+            inputValue:''
+        };
+    }
     render(){
         let {article} = this.props;
+        let {inputValue} = this.state;
         return (
-            <div style={{
-                width:'1100px',
-                margin:'0 auto'
-            }}>
-                {article ? <div dangerouslySetInnerHTML={{__html:article}}></div> : 'loading....'}
+            <div>
+                <input type="text" value={inputValue} onChange={this.textChange}/>
+                <p><b>{inputValue}</b></p>
+                <div style={{
+                    width: '1100px',
+                    margin: '20px auto'
+                }}>
+                    {article ? <div dangerouslySetInnerHTML={{ __html: article }}></div> : 'loading....'}
+                </div>
             </div>
+            
         );
     }
+
+    textChange(e){
+        this.setState({
+            inputValue: e.target.value
+        });
+    }
+    
     componentDidMount(){
         let {getArticle} = this.props;
         getArticle();
